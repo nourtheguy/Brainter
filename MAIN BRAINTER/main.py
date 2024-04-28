e77a3d9d8aad0322f9c182f50df71e95cd55dbf6
nourtheguy <nour.zeineddine11@gmail.com>
2024-04-28 22:08:54 +0300

    pen colors

--- MAIN BRAINTER/main.py
+++ MAIN BRAINTER/main.py
@@ -6,19 +6,19 @@ import time
 def main():
     start = time.time()
     # generates an image after receiving a text file from node red
-    print("Waiting for the necessary information")
-    # while True:
-    #     status, file_path = check_for_files()
-    #     if status == "command":
-    #         print("Start mental command process.")
-    #         process_mental_command(file_path)
-    #         print("Finished mental command process.")
-    #         break
-    #     elif status == "emotion":
-    #         print("Start emotion color matching process.")
-    #         process_emotion(file_path)
-    #         print("Finished emotion color matching  process.")
-    #         break
+    # print("Waiting for the necessary information")
+    # # while True:
+    # #     status, file_path = check_for_files()
+    # #     if status == "command":
+    # #         print("Start mental command process.")
+    # #         process_mental_command(file_path)
+    # #         print("Finished mental command process.")
+    # #         break
+    # #     elif status == "emotion":
+    # #         print("Start emotion color matching process.")
+    # #         process_emotion(file_path)
+    # #         print("Finished emotion color matching  process.")
+    # #         break
     TopG()
     end = time.time()
     print("Total time taken = " + str(end - start) + " seconds")
